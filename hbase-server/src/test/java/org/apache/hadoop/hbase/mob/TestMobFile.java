a7ac7bc8e43fcccd411a7d27c3dd8294b7c80ee4
plusplusjiajia <jiajia.li@intel.com>
2014-08-07 15:19:27 +0800

    add the mob core code UTs.

---
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/mob/TestMobFile.java
@@ -0,0 +1,126 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.mob;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseTestCase;
+import org.apache.hadoop.hbase.HBaseTestingUtility;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.KeyValue.Type;
+import org.apache.hadoop.hbase.KeyValueUtil;
+import org.apache.hadoop.hbase.SmallTests;
+import org.apache.hadoop.hbase.io.hfile.CacheConfig;
+import org.apache.hadoop.hbase.io.hfile.HFileContext;
+import org.apache.hadoop.hbase.io.hfile.HFileContextBuilder;
+import org.apache.hadoop.hbase.regionserver.BloomType;
+import org.apache.hadoop.hbase.regionserver.StoreFile;
+import org.apache.hadoop.hbase.regionserver.StoreFileScanner;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(SmallTests.class)
+public class TestMobFile extends HBaseTestCase {
+  static final Log LOG = LogFactory.getLog(TestMobFile.class);
+  private static final HBaseTestingUtility TEST_UTIL = new HBaseTestingUtility();
+  private CacheConfig cacheConf =  new CacheConfig(TEST_UTIL.getConfiguration());
+  private final String TABLE = "tableName";
+  private final String FAMILY = "familyName";
+
+  @Test
+  public void testReadKeyValue() throws Exception {
+    Path outputDir = new Path(new Path(this.testDir, TABLE), FAMILY);
+    HFileContext meta = new HFileContextBuilder().withBlockSize(8*1024).build();
+    StoreFile.Writer writer = new StoreFile.WriterBuilder(conf, cacheConf, this.fs)
+            .withOutputDir(outputDir)
+            .withFileContext(meta)
+            .build();
+    String caseName = getName();
+    MobTestUtil.writeStoreFile(writer, caseName);
+
+    MobFile mobFile = new MobFile(new StoreFile(this.fs, writer.getPath(),
+        conf, cacheConf, BloomType.NONE));
+    byte[] family = Bytes.toBytes(caseName);
+    byte[] qualify = Bytes.toBytes(caseName);
+
+    // Test the start key
+    byte[] startKey = Bytes.toBytes("aa");  // The start key bytes
+    KeyValue expectedKey = new KeyValue(startKey, family, qualify, Long.MAX_VALUE, Type.Put, startKey);
+    KeyValue seekKey = expectedKey.createKeyOnly(false);
+    KeyValue kv = KeyValueUtil.ensureKeyValue(mobFile.readCell(seekKey, false));
+    assertKeyValuesEquals(expectedKey, kv);
+
+    // Test the end key
+    byte[] endKey = Bytes.toBytes("zz");  // The end key bytes
+    expectedKey = new KeyValue(endKey, family, qualify, Long.MAX_VALUE, Type.Put, endKey);
+    seekKey = expectedKey.createKeyOnly(false);
+    kv = KeyValueUtil.ensureKeyValue(mobFile.readCell(seekKey, false));
+    assertKeyValuesEquals(expectedKey, kv);
+
+    // Test the random key
+    byte[] randomKey = Bytes.toBytes(MobTestUtil.generateRandomString(2));
+    expectedKey = new KeyValue(randomKey, family, qualify, Long.MAX_VALUE, Type.Put, randomKey);
+    seekKey = expectedKey.createKeyOnly(false);
+    kv = KeyValueUtil.ensureKeyValue(mobFile.readCell(seekKey, false));
+    assertKeyValuesEquals(expectedKey, kv);
+
+    // Test the key which is less than the start key
+    byte[] lowerKey = Bytes.toBytes("a1"); // Smaller than "aa"
+    expectedKey = new KeyValue(startKey, family, qualify, Long.MAX_VALUE, Type.Put, startKey);
+    seekKey = new KeyValue(lowerKey, family, qualify, Long.MAX_VALUE, Type.Put, lowerKey);
+    kv = KeyValueUtil.ensureKeyValue(mobFile.readCell(seekKey, false));
+    assertKeyValuesEquals(expectedKey, kv);
+
+    // Test the key which is more than the end key
+    byte[] upperKey = Bytes.toBytes("z{"); // Bigger than "zz"
+    seekKey = new KeyValue(upperKey, family, qualify, Long.MAX_VALUE, Type.Put, upperKey);
+    kv = KeyValueUtil.ensureKeyValue(mobFile.readCell(seekKey, false));
+    assertNull(kv);
+  }
+
+  /**
+   * Compare two KeyValue only for their row family qualifier value
+   */
+  @SuppressWarnings("deprecation")
+  private void assertKeyValuesEquals(KeyValue firstKeyValue,
+      KeyValue secondKeyValue) {
+    assertEquals(firstKeyValue.getRow(), secondKeyValue.getRow());
+    assertEquals(firstKeyValue.getFamily(), secondKeyValue.getFamily());
+    assertEquals(firstKeyValue.getQualifier(), secondKeyValue.getQualifier());
+    assertEquals(firstKeyValue.getValue(), secondKeyValue.getValue());
+  }
+
+  @Test
+  public void testGetScanner() throws Exception {
+    Path outputDir = new Path(new Path(this.testDir, TABLE), FAMILY);
+    HFileContext meta = new HFileContextBuilder().withBlockSize(8*1024).build();
+    StoreFile.Writer writer = new StoreFile.WriterBuilder(conf, cacheConf, this.fs)
+            .withOutputDir(outputDir)
+            .withFileContext(meta)
+            .build();
+    MobTestUtil.writeStoreFile(writer, getName());
+
+    MobFile mobFile = new MobFile(new StoreFile(this.fs, writer.getPath(),
+        conf, cacheConf, BloomType.NONE));
+    assertNotNull(mobFile.getScanner());
+    assertTrue(mobFile.getScanner() instanceof StoreFileScanner);
+  }
+}
---
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestMobFileStore.java
@@ -0,0 +1,143 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.regionserver;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HBaseTestCase;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.SmallTests;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.mob.MobConstants;
+import org.apache.hadoop.hbase.mob.MobUtils;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(SmallTests.class)
+public class TestMobFileStore extends HBaseTestCase {
+  public static final Log LOG = LogFactory.getLog(TestMobFileStore.class);
+
+  private MobFileStore mobFileStore;
+  private Path mobFilePath;
+  private FileSystem fs;
+  private KeyValue seekKey1;
+  private KeyValue seekKey2;
+  private KeyValue seekKey3;
+  private Date currentDate = new Date();
+
+  private final byte[] TABLE = Bytes.toBytes("table");
+  private final byte[] FAMILY = Bytes.toBytes("family");
+
+  private final byte[] ROW = Bytes.toBytes("row");
+  private final byte[] ROW2 = Bytes.toBytes("row2");
+  private final byte[] VALUE = Bytes.toBytes("value");
+  private final byte[] VALUE2 = Bytes.toBytes("value2");
+  private final byte[] QF1 = Bytes.toBytes("qf1");
+  private final byte[] QF2 = Bytes.toBytes("qf2");
+  private final byte[] QF3 = Bytes.toBytes("qf3");
+
+  private void init(String methodName) throws IOException {
+    init(methodName, HBaseConfiguration.create());
+  }
+
+  private void init(String methodName, Configuration conf) throws IOException {
+    HColumnDescriptor hcd = new HColumnDescriptor(FAMILY);
+    hcd.setMaxVersions(4);
+    hcd.setValue(MobConstants.IS_MOB, "true");
+    init(methodName, conf, hcd);
+  }
+
+  private void init(String methodName, Configuration conf, HColumnDescriptor hcd)
+      throws IOException {
+    // Setting up a Store
+    Path basedir = this.testDir;
+    fs = FileSystem.get(conf);
+
+    HTableDescriptor htd = new HTableDescriptor(TABLE);
+    htd.addFamily(hcd);
+    Path homePath = new Path(basedir, Bytes.toString(TABLE) + Path.SEPARATOR
+        + Bytes.toString(FAMILY));
+    fs.mkdirs(homePath);
+    mobFileStore = MobFileStore.create(conf, fs, TableName.valueOf(TABLE), hcd);
+
+    KeyValue key1 = new KeyValue(ROW, FAMILY, QF1, 1, VALUE);
+    KeyValue key2 = new KeyValue(ROW, FAMILY, QF2, 1, VALUE);
+    KeyValue key3 = new KeyValue(ROW2, FAMILY, QF3, 1, VALUE2);
+    KeyValue[] keys = new KeyValue[] { key1, key2, key3 };
+    int maxKeyCount = keys.length;
+    HRegionInfo regionStartKey = new HRegionInfo();
+    StoreFile.Writer mobWriter = mobFileStore.createWriterInTmp(currentDate,
+        maxKeyCount, hcd.getCompactionCompression(), regionStartKey.getStartKey());
+    mobFilePath = mobWriter.getPath();
+
+    mobWriter.append(key1);
+    mobWriter.append(key2);
+    mobWriter.append(key3);
+    mobWriter.close();
+    String targetPathName = MobUtils.formatDate(currentDate);
+    byte[] referenceValue =
+            Bytes.toBytes(targetPathName + Path.SEPARATOR
+                + mobFilePath.getName());
+    seekKey1 = new KeyValue(ROW, FAMILY, QF1, Long.MAX_VALUE, referenceValue);
+    seekKey2 = new KeyValue(ROW, FAMILY, QF2, Long.MAX_VALUE, referenceValue);
+    seekKey3 = new KeyValue(ROW2, FAMILY, QF3, Long.MAX_VALUE, referenceValue);
+  }
+
+  @Test
+  public void testCommitFile() throws Exception {
+    init(getName());
+    String targetPathName = MobUtils.formatDate(new Date());
+    Path targetPath = new Path(mobFileStore.getPath(), (targetPathName
+        + Path.SEPARATOR + mobFilePath.getName()));
+    fs.delete(targetPath);
+    assertFalse(fs.exists(targetPath));
+    //commit file
+    mobFileStore.commitFile(mobFilePath, targetPath);
+    assertTrue(fs.exists(targetPath));
+  }
+
+  @Test
+  @SuppressWarnings("deprecation")
+  public void testResolve() throws Exception {
+    init(getName());
+    String targetPathName = MobUtils.formatDate(currentDate);
+    Path targetPath = new Path(mobFileStore.getPath(), targetPathName);
+    mobFileStore.commitFile(mobFilePath, targetPath);
+    //resolve
+    Cell resultKey1 = mobFileStore.resolve(seekKey1, false);
+    Cell resultKey2 = mobFileStore.resolve(seekKey2, false);
+    Cell resultKey3 = mobFileStore.resolve(seekKey3, false);
+    //compare
+    assertEquals(resultKey1.getValue(), VALUE);
+    assertEquals(resultKey2.getValue(), VALUE);
+    assertEquals(resultKey3.getValue(), VALUE2);
+  }
+}
