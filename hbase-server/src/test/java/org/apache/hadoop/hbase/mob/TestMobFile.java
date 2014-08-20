83c0469e6af2ff05cbfa5bb399505f594cb3d458
plusplusjiajia <jiajia.li@intel.com>
2014-08-20 18:34:26 +0800

    refine the mob core test.

--- hbase-server/src/test/java/org/apache/hadoop/hbase/mob/TestMobFile.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/mob/TestMobFile.java
@@ -18,9 +18,14 @@
  */
 package org.apache.hadoop.hbase.mob;
 
+import junit.framework.TestCase;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HBaseTestCase;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.KeyValue;
@@ -34,29 +39,33 @@ import org.apache.hadoop.hbase.regionserver.BloomType;
 import org.apache.hadoop.hbase.regionserver.StoreFile;
 import org.apache.hadoop.hbase.regionserver.StoreFileScanner;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.FSUtils;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category(SmallTests.class)
-public class TestMobFile extends HBaseTestCase {
+public class TestMobFile extends TestCase {
   static final Log LOG = LogFactory.getLog(TestMobFile.class);
   private static final HBaseTestingUtility TEST_UTIL = new HBaseTestingUtility();
-  private CacheConfig cacheConf =  new CacheConfig(TEST_UTIL.getConfiguration());
+  private Configuration conf = TEST_UTIL.getConfiguration();
+  private CacheConfig cacheConf =  new CacheConfig(conf);
   private final String TABLE = "tableName";
   private final String FAMILY = "familyName";
 
   @Test
   public void testReadKeyValue() throws Exception {
-    Path outputDir = new Path(new Path(this.testDir, TABLE), FAMILY);
+    FileSystem fs = FileSystem.get(conf);
+	Path testDir = FSUtils.getRootDir(conf);
+    Path outputDir = new Path(new Path(testDir, TABLE), FAMILY);
     HFileContext meta = new HFileContextBuilder().withBlockSize(8*1024).build();
-    StoreFile.Writer writer = new StoreFile.WriterBuilder(conf, cacheConf, this.fs)
+    StoreFile.Writer writer = new StoreFile.WriterBuilder(conf, cacheConf, fs)
             .withOutputDir(outputDir)
             .withFileContext(meta)
             .build();
     String caseName = getName();
     MobTestUtil.writeStoreFile(writer, caseName);
 
-    MobFile mobFile = new MobFile(new StoreFile(this.fs, writer.getPath(),
+    MobFile mobFile = new MobFile(new StoreFile(fs, writer.getPath(),
         conf, cacheConf, BloomType.NONE));
     byte[] family = Bytes.toBytes(caseName);
     byte[] qualify = Bytes.toBytes(caseName);
@@ -67,28 +76,28 @@ public class TestMobFile extends HBaseTestCase {
         new KeyValue(startKey, family, qualify, Long.MAX_VALUE, Type.Put, startKey);
     KeyValue seekKey = expectedKey.createKeyOnly(false);
     KeyValue kv = KeyValueUtil.ensureKeyValue(mobFile.readCell(seekKey, false));
-    assertKeyValuesEquals(expectedKey, kv);
+    MobTestUtil.assertKeyValuesEquals(expectedKey, kv);
 
     // Test the end key
     byte[] endKey = Bytes.toBytes("zz");  // The end key bytes
     expectedKey = new KeyValue(endKey, family, qualify, Long.MAX_VALUE, Type.Put, endKey);
     seekKey = expectedKey.createKeyOnly(false);
     kv = KeyValueUtil.ensureKeyValue(mobFile.readCell(seekKey, false));
-    assertKeyValuesEquals(expectedKey, kv);
+    MobTestUtil.assertKeyValuesEquals(expectedKey, kv);
 
     // Test the random key
     byte[] randomKey = Bytes.toBytes(MobTestUtil.generateRandomString(2));
     expectedKey = new KeyValue(randomKey, family, qualify, Long.MAX_VALUE, Type.Put, randomKey);
     seekKey = expectedKey.createKeyOnly(false);
     kv = KeyValueUtil.ensureKeyValue(mobFile.readCell(seekKey, false));
-    assertKeyValuesEquals(expectedKey, kv);
+    MobTestUtil.assertKeyValuesEquals(expectedKey, kv);
 
     // Test the key which is less than the start key
     byte[] lowerKey = Bytes.toBytes("a1"); // Smaller than "aa"
     expectedKey = new KeyValue(startKey, family, qualify, Long.MAX_VALUE, Type.Put, startKey);
     seekKey = new KeyValue(lowerKey, family, qualify, Long.MAX_VALUE, Type.Put, lowerKey);
     kv = KeyValueUtil.ensureKeyValue(mobFile.readCell(seekKey, false));
-    assertKeyValuesEquals(expectedKey, kv);
+    MobTestUtil.assertKeyValuesEquals(expectedKey, kv);
 
     // Test the key which is more than the end key
     byte[] upperKey = Bytes.toBytes("z{"); // Bigger than "zz"
@@ -97,29 +106,19 @@ public class TestMobFile extends HBaseTestCase {
     assertNull(kv);
   }
 
-  /**
-   * Compare two KeyValue only for their row family qualifier value
-   */
-  @SuppressWarnings("deprecation")
-  private void assertKeyValuesEquals(KeyValue firstKeyValue,
-      KeyValue secondKeyValue) {
-    assertEquals(firstKeyValue.getRow(), secondKeyValue.getRow());
-    assertEquals(firstKeyValue.getFamily(), secondKeyValue.getFamily());
-    assertEquals(firstKeyValue.getQualifier(), secondKeyValue.getQualifier());
-    assertEquals(firstKeyValue.getValue(), secondKeyValue.getValue());
-  }
-
   @Test
   public void testGetScanner() throws Exception {
-    Path outputDir = new Path(new Path(this.testDir, TABLE), FAMILY);
+    FileSystem fs = FileSystem.get(conf);
+    Path testDir = FSUtils.getRootDir(conf);
+    Path outputDir = new Path(new Path(testDir, TABLE), FAMILY);
     HFileContext meta = new HFileContextBuilder().withBlockSize(8*1024).build();
-    StoreFile.Writer writer = new StoreFile.WriterBuilder(conf, cacheConf, this.fs)
+    StoreFile.Writer writer = new StoreFile.WriterBuilder(conf, cacheConf, fs)
             .withOutputDir(outputDir)
             .withFileContext(meta)
             .build();
     MobTestUtil.writeStoreFile(writer, getName());
 
-    MobFile mobFile = new MobFile(new StoreFile(this.fs, writer.getPath(),
+    MobFile mobFile = new MobFile(new StoreFile(fs, writer.getPath(),
         conf, cacheConf, BloomType.NONE));
     assertNotNull(mobFile.getScanner());
     assertTrue(mobFile.getScanner() instanceof StoreFileScanner);
