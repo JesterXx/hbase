162ad5999c959c57ad39dc8ab5a3f2e6e69099ce
plusplusjiajia <jiajia.li@intel.com>
2014-08-26 10:23:12 +0800

    add the license in test.

--- hbase-server/src/test/java/org/apache/hadoop/hbase/mob/TestMobDataBlockEncoding.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/mob/TestMobDataBlockEncoding.java
@@ -1,3 +1,21 @@
+/**
+*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package org.apache.hadoop.hbase.mob;
 
 import java.util.List;
@@ -55,7 +73,8 @@ public class TestMobDataBlockEncoding {
     TEST_UTIL.shutdownMiniCluster();
   }
 
-  public void setUp(String threshold, String TN, DataBlockEncoding encoding) throws Exception {
+  public void setUp(String threshold, String TN, DataBlockEncoding encoding)
+      throws Exception {
     desc = new HTableDescriptor(TableName.valueOf(TN));
     hcd = new HColumnDescriptor(family);
     hcd.setValue(MobConstants.IS_MOB, "true");
