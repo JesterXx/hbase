da03add947d7882d611d04145f2ee6adbad6b979
Jingcheng Du <jingcheng.du@intel.com>
2015-10-12 16:06:59 +0800

    Fix the test issue.

--- hbase-server/src/test/java/org/apache/hadoop/hbase/mob/compactions/TestMobCompactor.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/mob/compactions/TestMobCompactor.java
@@ -26,6 +26,7 @@ import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
@@ -78,6 +79,8 @@ import org.apache.hadoop.hbase.security.User;
 import org.apache.hadoop.hbase.testclassification.LargeTests;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.util.EnvironmentEdgeManager;
+import org.apache.hadoop.hbase.util.Pair;
+import org.apache.hadoop.hbase.util.SortedList;
 import org.apache.hadoop.hbase.util.Threads;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -153,10 +156,6 @@ public class TestMobCompactor {
   @Test(timeout = 300000)
   public void testMinorCompaction() throws Exception {
     resetConf();
-    int mergeSize = 5000;
-    // change the mob compaction merge size
-    conf.setLong(MobConstants.MOB_COMPACTION_MERGEABLE_THRESHOLD, mergeSize);
-
     // create a table with namespace
     NamespaceDescriptor namespaceDescriptor = NamespaceDescriptor.create("ns").build();
     String tableNameAsString = "ns:testMinorCompaction";
@@ -174,7 +173,10 @@ public class TestMobCompactor {
     assertEquals("Before deleting: mob file count", regionNum * count,
       countFiles(tableName, true, family1));
 
-    int largeFilesCount = countLargeFiles(mergeSize, tableName, family1);
+    Pair<Integer, Long> largerFiles = countLargeFiles(tableName, family1);
+    int largeFilesCount = largerFiles.getFirst();
+    // change the mob compaction merge size
+    conf.setLong(MobConstants.MOB_COMPACTION_MERGEABLE_THRESHOLD, largerFiles.getSecond());
     createDelFile(table, tableName, Bytes.toBytes(family1), Bytes.toBytes(qf1));
 
     assertEquals("Before compaction: mob rows count", regionNum * (rowNumPerRegion - delRowNum),
@@ -200,7 +202,8 @@ public class TestMobCompactor {
       * (cellNumPerRow * rowNumPerRegion - delCellNum), countMobCells(table));
     // After the compaction, the files smaller than the mob compaction merge size
     // is merge to one file
-    assertEquals("After compaction: family1 mob file count", largeFilesCount + regionNum,
+    assertEquals("After compaction: family1 mob file count",
+      (largeFilesCount == regionNum * count) ? largeFilesCount : largeFilesCount + regionNum,
       countFiles(tableName, true, family1));
     assertEquals("After compaction: family2 mob file count", regionNum * count,
       countFiles(tableName, true, family2));
@@ -593,27 +596,34 @@ public class TestMobCompactor {
     return count;
   }
 
-  /**
-   * Gets the number of files.
-   * @param size the size of the file
-   * @param tableName the current table name
-   * @param familyName the family name
-   * @return the number of files large than the size
-   */
-  private int countLargeFiles(int size, TableName tableName, String familyName) throws IOException {
+  private Pair<Integer, Long> countLargeFiles(TableName tableName, String familyName)
+    throws IOException {
     Path mobDirPath = MobUtils.getMobFamilyPath(MobUtils.getMobRegionPath(conf, tableName),
       familyName);
-    int count = 0;
+    SortedList<Long> fileLength = new SortedList<Long>(new Comparator<Long>() {
+      @Override
+      public int compare(Long o1, Long o2) {
+        return o1 == o2 ? 0 : (o1 > o2 ? 1 : -1);
+      }
+    });
     if (fs.exists(mobDirPath)) {
       FileStatus[] files = fs.listStatus(mobDirPath);
       for (FileStatus file : files) {
         // ignore the del files in the mob path
-        if ((!StoreFileInfo.isDelFile(file.getPath())) && (file.getLen() > size)) {
-          count++;
+        if (!StoreFileInfo.isDelFile(file.getPath())) {
+          fileLength.add(file.getLen());
         }
       }
     }
-    return count;
+    int index = fileLength.size() / 2;
+    if (index > 0) {
+      while (index < fileLength.size() && fileLength.get(index - 1) == fileLength.get(index)) {
+        index++;
+      }
+    }
+    return index == fileLength.size() ? new Pair<Integer, Long>(fileLength.size(),
+      fileLength.get(0))
+      : new Pair<Integer, Long>(fileLength.size() - index, fileLength.get(index));
   }
 
   /**
